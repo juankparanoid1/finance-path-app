261c3f0e49f5173d8ef7dba04d520b325ace489e
juan.calderon <jccalderon97@gmail.com>
2023-10-08 17:13:36 -0600

    add transaction screen is now complete

--- app/screens/transactions/Transactions.js
+++ app/screens/transactions/Transactions.js
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import TransactionsForm from '../../components/transactionsForm/TransactionsForm';
 
 const Transactions = () => {
+  const insets = useSafeAreaInsets();
   return (
-    <View>
-      <Text>Transactions</Text>
+    <View style={[styles.container, { paddingTop: insets.top, paddingBottom: insets.bottom }]}>
+      <TransactionsForm></TransactionsForm>
     </View>
   )
 }
 
 export default Transactions
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+  },
+})
